0e7adfe3b3b946def2926d401444e6b0099cc394
Andrei Petrescu <petrescuandrei2018@gmail.com>
2025-02-28 17:51:10 +0200

    Inițializare proiect ASP.NET Core MVC

---
+++ CarbuniGratar.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CarbuniGratar.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
